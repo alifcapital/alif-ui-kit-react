5fc5857c59a4768187e4a764154d27fdb73a4a1e
ManuchehrTilloev <manuchehr.tilloev@gmail.com>
2023-10-20 10:27:16 +0500

    add test on closing popover content on escape

--- src/components/Popover/tests/Popover.test.tsx
+++ src/components/Popover/tests/Popover.test.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { Bell } from 'alif-icon-kit-react';
 import { Popover } from '../Popover';
 
@@ -40,5 +40,31 @@ describe('Popover', () => {
     const element = await findByText(popoverContent);
 
     expect(element.textContent).toBe(popoverContent);
+  });
+
+  it('should close popover content', async () => {
+    const TestComponent = () => {
+      const [open, setOpen] = useState(true);
+
+      return (
+        <Popover
+          open={open}
+          onOpenChange={() => setOpen((prev) => !prev)}
+          ariaLabel="notification label"
+          triggerElement={triggerElement}
+        >
+          {popoverContent}
+        </Popover>
+      );
+    };
+
+    const { queryByText } = render(<TestComponent />);
+
+    fireEvent.keyDown(document, { key: 'Escape' });
+
+    await waitFor(() => {
+      const contentElement = queryByText(popoverContent);
+      expect(contentElement).not.toBeInTheDocument();
+    });
   });
 });
