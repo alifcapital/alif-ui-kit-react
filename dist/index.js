e070935584bd5e3b73d0a5eacd3c0d8b2bbaef9a
ManuchehrTilloev <manuchehr.tilloev@gmail.com>
2023-10-23 10:24:52 +0500

    build

--- dist/index.js
+++ dist/index.js
@@ -4795,8 +4795,8 @@ const cc = (e) => {
     popoverClassName: c,
     contentClassName: s,
     noStyle: l = !1,
-    sideOffset: u = 8,
-    align: v = "center"
+    sideOffset: u = 18,
+    align: v = "end"
   } = d, p = oe(d, [
     "open",
     "onOpenChange",
