cbfd3d69d726949d3313ebfe34d929257801b02d
Odinaev Abdurahmon <aodinaev000@gmail.com>
2023-11-17 11:51:07 +0500

    fix range type for ProgressBar

--- src/components/ProgressBar/ProgressBarTypes.ts
+++ src/components/ProgressBar/ProgressBarTypes.ts
@@ -1,14 +1,5 @@
 import { PROGRESS_BAR_SIZE } from './ProgressBarConstants';
 
-type NumericRange<
-  START extends number,
-  END extends number,
-  ARR extends unknown[] = [],
-  ACC extends number = never,
-> = ARR['length'] extends END
-  ? ACC | START | END
-  : NumericRange<START, END, [...ARR, 1], ARR[START] extends undefined ? ACC : ACC | ARR['length']>;
-
 type TProgressBarSize = `${PROGRESS_BAR_SIZE}`;
 
 export interface IProgressBar {
@@ -19,5 +10,5 @@ export interface IProgressBar {
 }
 
 export interface IProgressBarProps extends Partial<IProgressBar> {
-  progress: NumericRange<0, 100>;
+  progress: number;
 }
