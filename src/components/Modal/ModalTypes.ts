3d75a61ec001646655de4e03115169e4034e3906
ManuchehrTilloev <manuchehr.tilloev@gmail.com>
2023-11-21 19:03:23 +0500

    add contentClassName and refactor

--- src/components/Modal/ModalTypes.ts
+++ src/components/Modal/ModalTypes.ts
@@ -2,6 +2,7 @@ export interface IModalProps {
   open: boolean;
   onOpenChange: () => void;
   children: React.ReactNode;
+  contentClassName?: string;
   disableBackdropClick?: boolean;
   disableEscapeKey?: boolean;
 }
