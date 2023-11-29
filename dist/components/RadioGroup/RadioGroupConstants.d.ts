33d1323cffd83af09caa81aa5a3ae1b1840af741
Romesh28 <romesh280301@mail.ru>
2023-11-29 14:52:03 +0500

    build

---
+++ dist/components/RadioGroup/RadioGroupConstants.d.ts
@@ -0,0 +1,14 @@
+export declare enum RADIO_GROUP_THEME {
+    Light = "light",
+    Dark = "dark",
+    Green = "green"
+}
+export declare enum RADIO_GROUP_SIZE {
+    Small = "small",
+    Medium = "medium",
+    Large = "large"
+}
+export declare enum RADIO_GROUP_VARIANT {
+    row = "row",
+    column = "column"
+}
