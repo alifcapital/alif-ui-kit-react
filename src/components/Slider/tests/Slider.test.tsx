fad390ab05d40385d53f3f9d385a570ee83abcd7
eneegmatic <84386965+eneegmatic@users.noreply.github.com>
2023-12-04 17:02:05 +0500

    feat: cli for auto creating ui component folder (#67)

--- src/components/Slider/tests/Slider.test.tsx
+++ src/components/Slider/tests/Slider.test.tsx
@@ -6,7 +6,9 @@ import { Slider } from '..';
 describe('Slider', () => {
   it('should change value on user interaction', () => {
     const handleChange = jest.fn();
-    const { getByRole } = render(<Slider ariaLabel="Slider label" onChange={handleChange} />);
+    const { getByRole } = render(
+      <Slider value={0} ariaLabel="Slider label" onChange={handleChange} />,
+    );
 
     const slider = getByRole('slider');
     fireEvent.change(slider, { target: { value: 30 } });
