e4d09d79c60136fd9163895c5ee5cc885e43ffb0
ManuchehrTilloev <manuchehr.tilloev@gmail.com>
2024-01-29 10:28:06 +0500

    fix dropdown

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import { PopoverContent, PopoverTrigger, Root } from '@radix-ui/react-popover';
+import { PopoverContent, PopoverTrigger, Portal, Root } from '@radix-ui/react-popover';
 import clsx from 'clsx';
 import { IPopoverProps } from './PopoverTypes';
 import './PopoverStyle.scss';
 
+const Content = (props: Partial<IPopoverProps>) => {
+  const {
+    children,
+    contentClassName,
+    noStyle = false,
+    sideOffset = 18,
+    align = 'end',
+    ...restProps
+  } = props;
+
+  return (
+    <PopoverContent
+      align={align}
+      sideOffset={sideOffset}
+      className={clsx({
+        [contentClassName || '']: !!contentClassName,
+        ['Popover-content']: !!contentClassName || !noStyle,
+      })}
+      {...restProps}
+    >
+      {children}
+    </PopoverContent>
+  );
+};
+
 export const Popover = (props: IPopoverProps) => {
   const {
+    isPortal = true,
     open,
     onOpenChange,
     onTriggerClick,
@@ -13,10 +39,7 @@ export const Popover = (props: IPopoverProps) => {
     triggerElement,
     children,
     popoverClassName,
-    contentClassName,
     noStyle = false,
-    sideOffset = 18,
-    align = 'end',
     triggerTabindex,
     triggerAsChild = false,
     ...restProps
@@ -42,19 +65,15 @@ export const Popover = (props: IPopoverProps) => {
           </PopoverTrigger>
         )}
 
-        {open && children && (
-          <PopoverContent
-            align={align}
-            sideOffset={sideOffset}
-            className={clsx({
-              [contentClassName || '']: !!contentClassName,
-              ['Popover-content']: !!contentClassName || !noStyle,
-            })}
-            {...restProps}
-          >
-            {children}
-          </PopoverContent>
-        )}
+        {open &&
+          children &&
+          (isPortal ? (
+            <Portal>
+              <Content {...restProps}>{children}</Content>
+            </Portal>
+          ) : (
+            <Content {...restProps}>{children}</Content>
+          ))}
       </Root>
     </div>
   );
